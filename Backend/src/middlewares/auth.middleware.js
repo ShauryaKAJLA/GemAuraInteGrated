b54167150015e4f157b2ce2949d67c0a1b949d59
ShauryaKAJLA <126277304+ShauryaKAJLA@users.noreply.github.com>
2024-07-04 13:58:23 +0530

    Update auth.middleware.js

--- Backend/src/middlewares/auth.middleware.js
+++ Backend/src/middlewares/auth.middleware.js
@@ -8,7 +8,7 @@ const verifyJWT=asyncHandler(async (req,res,next)=>{
     try {
         console.log("Mujhe kyu chlaya")
         const token=req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
-        console.log(req.cookie)
+        console.log(req)
         if(!token)
             {
                 return res.status(400).json(new apiError(400,"Token not found"))
